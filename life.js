3879c36838c0dd81f2454db5103ca3db486a0e7d
Schwannbayo <schwannbayo@gmail.com>
2022-06-24 09:49:14 +0100

    initial commit

---
+++ life.js
@@ -0,0 +1,6 @@
+let yourName = prompt(' kindly input your name please : ')
+
+let yourHeight = prompt( ' input your height ')
+
+let yourCountry = prompt ( ' which country are you from? ')
+alert( 'My name is ' + yourName  + ' ,my height is ' + yourHeight + ' and my country is ' + yourCountry )
